bc1680bb28c760546070b6e362c6e1176b39ef1d
Giulio Marin <giulio.marin@me.com>
2015-05-20 15:31:52 -0700

    load an xml file stored with OpenCV

---
+++ loadopencvxmlfile.m
@@ -0,0 +1,44 @@
+function [ content ] = loadopencvxmlfile( filepath )
+% LOADOPENCVXMLFILE( filepath )
+%
+% Load data in the xml file stored with OpenCV.
+%
+% INPUT
+%
+% filePath:     Path to the OpenCV xml file.
+%
+% OUTPUT
+%
+% content:     Structure with data in the file.
+
+% Giulio Marin
+%
+% giulio.marin@me.com
+% 2015/05/15
+
+%% Parse file
+param = parseXML(filepath);
+
+for moduleIdx = 2:2:size(param.Children, 2) - 1
+        name = param.Children(moduleIdx).Name;
+        if size(param.Children(moduleIdx).Children, 2) >= 8
+            % Is a Mat
+            rows = str2double(param.Children(moduleIdx).Children(2).Children.Data);
+            cols = str2double(param.Children(moduleIdx).Children(4).Children.Data);
+            dataRaw = param.Children(moduleIdx).Children(8).Children.Data;
+            data = reshape(strread(dataRaw, '%f'), [cols, rows])';
+        else
+            % Is a single value
+            % If not a string, convert to double
+            data = param.Children(moduleIdx).Children.Data;
+            if ~strcmp(data(1), '"')
+                data = str2double(param.Children(moduleIdx).Children.Data);
+            end
+        end
+        
+        % Add new entry
+        content.(name) = data;
+end
+
+end
+
