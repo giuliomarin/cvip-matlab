e4e7a92054695ab79fb4d21a8576ac9f62a0b74f
Giulio Marin <giulio.marin@me.com>
2015-10-27 10:51:26 +0100

    fix for nested nodes

--- loadopencvxmlfile.m
+++ loadopencvxmlfile.m
@@ -21,28 +21,43 @@ function [ content ] = loadopencvxmlfile( filepath )
 % 2015/05/15
 
 %% Parse file
-param = parsexml(filepath);
 
-for moduleIdx = 2:2:size(param.Children, 2) - 1
-        name = param.Children(moduleIdx).Name;
-        if size(param.Children(moduleIdx).Children, 2) >= 8
-            % Is a Mat
-            rows = str2double(param.Children(moduleIdx).Children(2).Children.Data);
-            cols = str2double(param.Children(moduleIdx).Children(4).Children.Data);
-            dataRaw = param.Children(moduleIdx).Children(8).Children.Data;
-            data = reshape(strread(dataRaw, '%f'), [cols, rows])';
-        else
-            % Is a single value or a sequence
-            % If not a string, convert to double
-            data = param.Children(moduleIdx).Children.Data;
-            if ~strcmp(data(1), '"')
-                data = str2num(param.Children(moduleIdx).Children.Data);
-            end
+param = parsexml(filepath);
+content = parseChildren(param);
+
+end
+
+% Recursively get nodes
+function content = parseChildren(node)
+for moduleIdx = 1:size(node.Children, 2)
+    if numel(node.Children(moduleIdx).Children) == 0
+        % Is an empty node
+        
+        continue
+    elseif numel(node.Children(moduleIdx).Children) == 1
+        % Is a single value or a sequence
+        
+        data = node.Children(moduleIdx).Children.Data;
+        % If not a string, convert to double
+        if ~strcmp(data(1), '"')
+            data = str2num(node.Children(moduleIdx).Children.Data);
         end
         
         % Add new entry
-        content.(name) = data;
+        content.(node.Children(moduleIdx).Name) = data;
+    elseif numel(node.Children(moduleIdx).Children) == 8 && (str2double(node.Children(moduleIdx).Children(2).Children.Data) > 0) && (str2double(node.Children(moduleIdx).Children(4).Children.Data) > 0)
+        % Is a matrix
+        
+        rows = str2double(node.Children(moduleIdx).Children(2).Children.Data);
+        cols = str2double(node.Children(moduleIdx).Children(4).Children.Data);
+        dataRaw = node.Children(moduleIdx).Children(8).Children.Data;
+        data = reshape(strread(dataRaw, '%f'), [cols, rows])';
+        
+        % Add new entry
+        content.(node.Children(moduleIdx).Name) = data;
+    else
+        contentChildren = parseChildren(node.Children(moduleIdx));
+        content.(node.Children(moduleIdx).Name) = contentChildren;
+    end
 end
-
 end
-
